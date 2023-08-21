a3edcaee45040028f08f453dd9a3d21c3fb2d21d
Guillermo Villacura <gu.villacura@gmail.com>
2023-08-21 17:53:09 -0400

    Clase 21-08-2023

--- myFirstApp/capacitor.config.ts
+++ myFirstApp/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'cl.vilitu.app',
   appName: 'myFirstApp',
   webDir: 'www',
   server: {
--- myFirstApp/src/app/pages/menu/menu.page.ts
+++ myFirstApp/src/app/pages/menu/menu.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AnimationController, IonCard } from '@ionic/angular';
+import type { Animation } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu',
@@ -9,9 +10,39 @@ import { Router } from '@angular/router';
 })
 export class MenuPage implements OnInit {
 
-  constructor(private router:Router) { }
+
+  @ViewChild(IonCard, { read: ElementRef })
+  card!: ElementRef<HTMLIonCardElement>;
+
+  private animation!: Animation;
+
+  constructor(private router:Router, private animationCtrl: AnimationController) { }
 
   ngOnInit() {
+  }
+
+
+  ngAfterViewInit() {
+    this.animation = this.animationCtrl
+      .create()
+      .addElement(document.querySelectorAll("ion-card"))
+      .duration(1500)
+      .iterations(Infinity)
+      .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
+      .fromTo('opacity', '1', '0.2');
+  }
+
+
+  play(){
+    this.animation.play();
+  }
+
+  pause(){
+    this.animation.pause();
+  }
+
+  stop(){
+    this.animation.stop();
   }
 
 
