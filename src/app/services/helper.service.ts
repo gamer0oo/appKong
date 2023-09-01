d44a794457b1d02c28bf8a06e5a9d5ca78e1d8de
ElJano <al.penailillo@hotmail.com>
2023-08-31 22:09:09 -0400

    create login page

---
+++ src/app/services/helper.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HelperService {
+
+  constructor(private alertService:AlertController) { }
+
+  async showAlert(mensaje:string, titulo:string) {
+    let alert = await this.alertService.create({cssClass:"alertClass", message:mensaje, header:titulo, buttons:['Aceptar']})
+  }
+}
