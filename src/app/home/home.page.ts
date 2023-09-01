f4139c989b93347dd61e8d49c3db58539b4f066e
Macarena Muñoz <macaigm@gmail.com>
2023-08-31 22:42:32 -0400

    add working user url params

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,19 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  username : string = '';
+  
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe((params: ParamMap) => {
       const usuario = params.get('usuario');
-      this.nombreUsuario = usuario;
+      this.username = usuario ? usuario : '';
     });
-    
   }
 }
